52719f6cb174fa74ef474caf409017d76c6a5e53
Tatetsu <83476423+Tatetsu@users.noreply.github.com>
2022-03-18 15:25:43 +0900

    テンプレート構文

--- src/index.js
+++ src/index.js
@@ -1,10 +1,80 @@
-import "./styles.css";
+// const name = "じゃけぇ";
+// const age = 28;
+// //「私の名前はじゃけぇです。年齢は28歳です。」
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。"
+// console.log(message1)
+
+// // テンプレート文字列を用いる方法
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//　従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => {
+  return str;
+};
+console.log(func2("func2です"));
+
+// const name = "じゃけぇ";
+// const age = 28;
+// //「私の名前はじゃけぇです。年齢は28歳です。」
+
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です。"
+// console.log(message1)
+
+// // テンプレート文字列を用いる方法
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//　従来の関数
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// // アロー関数
+// const func2 = (str) => {
+//   return str;
+// }
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20));
+
+/**
+ *  分割代入
+ */
+// const myPro
+// const myProfile = [`じゃけぇ`, 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。}`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です`;
+// console.log(message4);
+
+const sayHello = (name) => console.log(`こんにちは！${name}さん`);
+sayHello("じぇけぇ");
