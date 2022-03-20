726f15d88a12df8ae21355e7537b23a40fc8b124
Tatetsu <83476423+Tatetsu@users.noreply.github.com>
2022-03-20 11:44:39 +0900

    mapなどの復習

--- src/index.js
+++ src/index.js
@@ -17,16 +17,16 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
 // アロー関数
-const func2 = (str) => {
-  return str;
-};
-console.log(func2("func2です"));
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
 
 // const name = "じゃけぇ";
 // const age = 28;
@@ -114,3 +114,45 @@ console.log(func2("func2です"));
 // console.log(arr8)
 // arr8[0] = 100;
 // console.log(arr4)
+
+/**
+ * 三項演算子
+ */
+// ある条件　？　条件がtrueのとき　: 条件がfalseのとき
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1)
+
+// const num = 130000000;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください'
+// console.log(`${formattedNum}円です`)
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています！！' : '許容範囲内です';
+// }
+// console.log(checkSum(50, 60))
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if(flag1 || flag2) {
+//   console.log("1か２はtrueになります")
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1か２はtrueになります")
+// }
+
+// || は左側がfalseなら右側を返す (左側がtrueなら左側を返す)
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee)
+
+// &&　は左側がtrueなら右側を返す （左側がfalseなら左側を返す）
+const num2 = null;
+const fee2 = num2 && "なにか設定されました";
+console.log(fee2);
